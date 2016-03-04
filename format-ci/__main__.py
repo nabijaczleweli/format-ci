3cc43d8f7b2b5cd85afc4cd60b99a5eb87ba7781
nabijaczleweli <nabijaczleweli@gmail.com>
2016-03-04 14:48:43 +0100

    Add assets and templated webpage base

--- format-ci/__main__.py
+++ format-ci/__main__.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
 
-from flask import Flask
+from flask import Flask, url_for, render_template
+import sqlite3
 import os
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../assets")
 
 
 @app.route("/")
 def hello():
 	return "Hello World!"
+
+@app.route("/blerb")
+def blerb():
+	return render_template("home.html")
 
 
 if __name__ == "__main__":
