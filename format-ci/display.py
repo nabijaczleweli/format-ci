b9786edc1b5dad86b4e7afddec514d58c3479bd8
nabijaczleweli <nabijaczleweli@gmail.com>
2016-03-05 11:46:51 +0100

    Fix job links. Add intro section to home

--- format-ci/display.py
+++ format-ci/display.py
@@ -4,6 +4,6 @@
 from flask import render_template
 
 
-def cook_html(projects):
+def cook_projects_html(projects):
 	return "".join(map(lambda pr: render_template("repo_box.html", repo_owner=pr[0], repo_name=pr[1], repo_passed=pr[2], repo_job_amount=pr[3],
 	                                              last_job_id=eval(pr[4])[-1]), projects))
